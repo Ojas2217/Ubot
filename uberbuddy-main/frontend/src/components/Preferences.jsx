a17416224ed852706d7a83803511c650f07c6c09
Ojas <ojasmuscat@gmail.com>
2025-10-05 01:21:19 +0530

    Delete uberbuddy-main directory
    bye bye

--- uberbuddy-main/frontend/src/components/Preferences.jsx
+++
@@ -1,9 +0,0 @@
-export default function Preferences(){
-    return(
-    <div className=' flex flex-col w-full  items-center h-full justify-center bg-[#303030] border-gray-500 border-b p-8 pace-y-10'>
-      <div>
-
-      </div>
-    </div>
-    )
-}
